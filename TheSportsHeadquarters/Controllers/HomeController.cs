43a19984646000a92ce64b199f1aeab8a24bc967
zjleaman <zjleaman@gmail.com>
2016-01-16 12:27:55 -0500

    First working check-in

--- TheSportsHeadquarters/Controllers/HomeController.cs
+++ TheSportsHeadquarters/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TheSportsHeadquarters.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Home");
         }
     }
 }
